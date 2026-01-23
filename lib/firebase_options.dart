2647e6005e5ff0098d910b465f535fd97605fe96
sanum <sanumonthomas321@gmail.com>
2026-01-23 11:05:32 +0530

    set something

---
+++ lib/firebase_options.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class AppFirebaseOptions {
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDflPKyUCxlMyv-y8dw_iao5afMNtqTAak',
+    appId: '1:3919305352:android:f547d14b6439a12f021d3d',
+    messagingSenderId: '3919305352',
+    projectId: 'zomato-9d26b',
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:goolez/firebase_options.dart';
 import 'package:goolez/screens/homepage.dart';
 import 'package:goolez/screens/loginpage.dart';
 import 'package:goolez/screens/registerpage.dart';
@@ -7,12 +8,10 @@ import 'package:goolez/screens/splashscreen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: FirebaseOptions(
-      apiKey: 'AIzaSyDflPKyUCxlMyv-y8dw_iao5afMNtqTAak',
-      appId: "1:3919305352:android:f547d14b6439a12f021d3d",
-      messagingSenderId: "3919305352",
-      projectId: "zomato-9d26b")
+  await Firebase.initializeApp(
+    options: AppFirebaseOptions.android,
   );
+
   runApp(const MyApp());
 }
 
